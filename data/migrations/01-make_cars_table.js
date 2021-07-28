706ac08503b10132b2a0d32e0c45ac582c2057ac
Kyle Naumann <67936151+kyle-naumann@users.noreply.github.com>
2021-07-27 20:31:28 -0700

    MVP

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,7 +1,18 @@
 exports.up = function (knex) {
   // DO YOUR MAGIC
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments();
+    tbl.string('make').notNullable();
+    tbl.string('vin').unique().notNullable();
+    tbl.string('model').notNullable();
+    tbl.integer('mileage').notNullable();
+    tbl.string('title');
+    tbl.string('transmission');
+});
 };
 
 exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('cars');
+
   // DO YOUR MAGIC
 };
